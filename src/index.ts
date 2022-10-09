f096089c46fef56c09d81247ff3b9247c1747ab8
jbalpert <jbalpert@umich.edu>
2022-10-09 14:14:09 -0400

    yes!

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors(
     {
-        origin: "https://app.umichbucketlist.com",
+        origin: "web-production-0817.up.railway.app",
         credentials: true,
     }
 ));
